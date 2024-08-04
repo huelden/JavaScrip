75301cfcf8d4c913bb774f4fe2249c50a4652348
huelden <156624639+huelden@users.noreply.github.com>
2024-08-04 17:29:50 +0100

    atençao rever os códigos

--- aula15 ex/script.js
+++ aula15 ex/script.js
@@ -1,57 +1,16 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else {
-       var fsex = document.getElementsByName('radsex')
-       var idade = ano - Number(fano.value)
-       res.innerHTML = `Idade calculada: ${idade}`
-       var genero = ''
-       var img = document.createElement('img')
-       img.setAttribute('id' , 'foto')
-       if (fsex[0].checked) {
-        genero = 'Homen'
-        if (idade >=0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'menino.jpg')
+function contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
 
-        }
-            
-        else if (idade  < 21) {
-            //jovem
-        }
-        else if (idade < 50) {
-            //adulto
-            img.setAttribute('src','homen.jpg')
-        }
-        else {
-            //idoso
-            img.setAttribute('src','idoso.jpg')
-        }
-      } else if(fsex[1].checked) {
-         genero = 'Mulher'
-         if (idade >=0 && idade < 10){
-            //criança
-            img.setAttribute('src', 'menina.jpg')
-         } 
-         else if (idade < 21) {
-            //jovem
-         }
-         else if (idade < 50) {
-            //adulto
-            img.setAttribute('src','mulher.jpg')
-         }
-         else {
-            //idosa
-            img.setAttribute('src', 'idosa.jpg')
-         }
-        
-        }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-        res.appendChild(img)
 
-    }
+    
+if (ini.value.length == 0 || fim.value.length == 0 || passo.Value.length == 0) { 
+    window.alert('[ERRO] Faltam dados!')
+} else {
+    res.innerHTML = `Contando`
+
+}
+     
 }
