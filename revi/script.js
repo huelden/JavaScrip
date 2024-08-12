218f0dd8f6f2fdb17b46db5bd7fd54877073f0d3
huelden <156624639+huelden@users.noreply.github.com>
2024-08-12 23:19:50 +0100

    projeto de revisão

--- revi/script.js
+++ revi/script.js
@@ -1,1 +1,21 @@
-alert('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minut = data.getMinutes()
+    //var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas e ${minut} minutos.`
+
+}
+if (hora >= 0 && hora <12) {
+    //BOM DIA!
+    img.src = 'img/manha.jpg'
+
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE!
+    img.src = 'img/tarde.jpg'
+} else {
+    //BOA NOITE!
+    img.src = 'img/noite.jpg'
+}
