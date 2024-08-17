0c478f2ca1eacbb201ce1b93b8aece67deb59dd9
huelden <156624639+huelden@users.noreply.github.com>
2024-08-17 12:47:28 +0100

    finalizando o ex de contagem

--- aula17/script.js
+++ aula17/script.js
@@ -1,5 +1,32 @@
 function contar(){
-    let ini
-    let fim
-    let passo
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO]')
+    
+}else{
+    res.innerHTML ='Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (i < f){
+        //contagem crescente
+         for (let c = i; c <= f; c += p){
+             res.innerHTML += ` ${c} \u{1F449}`
+     }
+        
+    } else {
+        //contagrem regressiva
+        for (let c = i; c >= f; c -= p ){
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        
+    }
+             res.innerHTML += ` \u{1F3C1}`
+    
+}
 }
