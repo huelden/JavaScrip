3384d2f3ab7d6be860ac7ec4189baccd9bcb2439
huelden <156624639+huelden@users.noreply.github.com>
2024-08-28 20:05:28 +0100

    rev let  var

---
+++ rev-js/javaScript.js
@@ -0,0 +1,22 @@
+/*document.getElementById("texto").innerHTML="Meu primeiro texto em <b>JS</b>!";
+console.log("Oi isso é um console.log!");*/
+
+
+
+/*
+let a, b ,c;
+a=5;
+b=6;
+c=7;
+
+pode ser tambem como
+
+let a, b, c; a=5; b=6; c=7;
+
+*/
+
+var a = 1;
+var b = 5;
+var c= a + b;
+console.log(c);
+
