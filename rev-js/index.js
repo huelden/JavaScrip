97c10c65c0f6d0a2f790f7a222d83d524fda47dc
huelden <156624639+huelden@users.noreply.github.com>
2024-08-28 23:09:31 +0100

    res de operações

---
+++ rev-js/index.js
@@ -0,0 +1,73 @@
+/* 
+Vamos enteder operadoes:
+-> Os operadores JavaScript são usados para atribuir valores
+, compara valores, executar operações aritmeticas
+e muito mais.
+Os sinais que usamos:
++ - * / = ++ -- += -= && || etc...
+São separdos em 6 "categorias."
+
+1) Operadores aritmeticos (matematicos)
+ex: 
+
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+
+total = valor1 + valor2;
+alert(total);
+
+====================================================
+2) Operadores de Atribuição
+var valor1, valor2, total;
+valor1 = 5;
+valor2 = 2;
+
+total += valor2;
+
+====================================================
+3) Operadores de Sequência 
+var valor1, valor2 total
+valor1 = "jose";
+valor = "carvalho";
+~
+total = valor1 + valor2;
+alert(total);
+
+====================================================
+4) Operadores de conparação
+
+var valor1, valor2 total
+valor1 = 8;
+valor = 10;
+
+total = (valor1 == valor2;) // tru (verdadeiro) ou false (falso)
+
+total = (valor1 === valor2;)// para ver se o valor é igual e se o tipo tambem é igual.
+total = (valor1 != valor2;) diferent ou não igual
+total = (valor1 < valor2;)maior ou menor q
+total = (valor1 > valor2;)maior ou menor q
+total = (valor1 >= valor2;)maior, menor ou igual
+total = (valor1 <= valor2;)maior, menor ou igual
+
+alert(total);
+====================================================================
+5) Operadores de condicional (ternario)
+
+var idade, eleitor:
+idade=16;
+eleitor=(idade<16) ? "Não, Eleitor":"Sim, Eleitor";
+alert('A resposta é: ' + eleitor + 'a idade dele é de: ' + idade);
+
+
+resultado = (idade >60 && idade<70); //true //false (É)
+resultado = (idade===60 || idade===70); //true //false (Óu)
+
+resultado = !(idade === 65); //true //false negação
+
+
+alert(resultado);
+
+6) Operadores logicos
+
+*/ 
--- rev-js/javaScript.js
+++ rev-js/javaScript.js
@@ -13,10 +13,43 @@ pode ser tambem como
 
 let a, b, c; a=5; b=6; c=7;
 
+
 */
 
-var a = 1;
-var b = 5;
-var c= a + b;
-console.log(c);
+
+/* Vamos enteder Vari´sveis
+Variáveis são "recipientes" onde podemos armazaner informações que podem variar, ou seja, podem ter qualquer tipo de valor.
+
+No JavaScript temos tres palavras-chaves para declarar variáveis:
+-> var
+-> let
+-> const 
+            DECLARAÇÃO DE VARIAVEIS
+var pote = "bombo";
+alert(pote);
+// // // //exemplo// // //
+var a =2;
+var b =3;
+var c =a + b;
+alert(c);
+
+ou pode ser assim:
+
+var a,b,c;
+a = 10;
+b = 3;
+c = a + b;
+alert(c);
+
+*/
+
+var nome, sobrenome, nomeCompleto;
+nome = "Jose";
+sobrenome = "Carvalho";
+nomeCompleto = nome +" "+ sobrenome;
+
+document.getElementById("texto").innerHTML = nomeCompleto;
+
+
+
 
